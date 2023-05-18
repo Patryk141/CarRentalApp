de1b8f103cff435236cb9e45e593e637776bd5c5
Ozilele <90649604+Ozilele@users.noreply.github.com>
2023-05-18 18:43:40 +0200

    Activities Update

--- app/src/main/java/com/example/carrental/LoginActivity.kt
+++ app/src/main/java/com/example/carrental/LoginActivity.kt
@@ -2,10 +2,41 @@ package com.example.carrental
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.gms.common.SignInButton
+import com.google.android.material.textfield.TextInputEditText
 
 class LoginActivity : AppCompatActivity() {
+
+  lateinit var loginBtn : Button
+  lateinit var loginWithGoogleBtn : SignInButton
+  lateinit var editTextName : TextInputEditText
+  lateinit var editTextPassword : TextInputEditText
+  lateinit var editTextPasswordRepeat : TextInputEditText
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_login)
+
+    loginBtn = findViewById(R.id.loginBtn)
+    loginWithGoogleBtn = findViewById(R.id.sign_in_button)
+
+    editTextName = findViewById(R.id.editTextName)
+    editTextPassword = findViewById(R.id.editTextPassword)
+    editTextPasswordRepeat = findViewById(R.id.editTextPasswordRepeat)
+
+    loginBtn.setOnClickListener{
+      Toast.makeText(this, "You attempt to log in", Toast.LENGTH_SHORT).show()
+      val name = editTextName.text
+      val password = editTextPassword.text
+      val passwordRepeat = editTextPasswordRepeat.text
+
+      if(password != passwordRepeat || password == null || passwordRepeat == null) {
+        Toast.makeText(this, "Problem with log in", Toast.LENGTH_SHORT).show()
+      }
+
+    }
+
   }
 }
--- app/src/main/java/com/example/carrental/RegisterActivity.kt
+++ app/src/main/java/com/example/carrental/RegisterActivity.kt
@@ -3,6 +3,8 @@ package com.example.carrental
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.core.widget.doAfterTextChanged
 import androidx.core.widget.doOnTextChanged
 import com.google.android.material.textfield.TextInputEditText
@@ -11,18 +13,71 @@ import com.google.android.material.textfield.TextInputLayout
 class RegisterActivity : AppCompatActivity() {
 
   lateinit var textInputLayoutEmail : TextInputLayout
-  lateinit var editTextEmail : TextInputEditText
+  lateinit var textInputLayName : TextInputLayout
+  lateinit var textInputLayPassword : TextInputLayout
+  lateinit var textInputLayPhone : TextInputLayout
+  lateinit var registerBtn : Button
+  lateinit var alreadyregisteredBtn : Button
+  lateinit var editTextEmail  : TextInputEditText
+  lateinit var editTextName : TextInputEditText
+  lateinit var editTextPhone : TextInputEditText
+  lateinit var editTextPassword : TextInputEditText
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_register)
 
+    registerBtn = findViewById(R.id.registerBtn)
+    alreadyregisteredBtn = findViewById(R.id.btnAccAlready)
+
     textInputLayoutEmail = findViewById(R.id.textInputLayEmail)
+    textInputLayName = findViewById(R.id.textInputLayName)
+    textInputLayPassword = findViewById(R.id.textInputLayPassword)
+    textInputLayPhone = findViewById(R.id.textInputLayPhone)
+
     editTextEmail = findViewById(R.id.editTextEmail)
+    editTextName = findViewById(R.id.editTextName)
+    editTextPhone = findViewById(R.id.editTextPhone)
+    editTextPassword = findViewById(R.id.editTextPassword)
 
     editTextEmail.doAfterTextChanged {
       println("${it.toString()}")
     }
+
+    registerBtn.setOnClickListener{
+      var name = editTextName.text
+      val email = editTextEmail.text
+      val password = editTextPassword.text
+      val phoneNum = editTextPhone.text
+
+      if(password == null || password.length < 4) {
+        textInputLayPassword.error = "Invalid password(min 4 sybmols)"
+      }
+
+      if(phoneNum!!.length < 9 || phoneNum == null) {
+        textInputLayPhone.error = "Invalid phone number(9 numbers)"
+      }
+
+      if(name == null) {
+        textInputLayName.error = "Invalid field"
+      }
+      if(email == null || !email.contains("@")) {
+        textInputLayoutEmail.error = "Invalid email"
+      } else {
+        Toast.makeText(this, "You are registered", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+      }
+    }
+
+    alreadyregisteredBtn.setOnClickListener{
+      Toast.makeText(this, "You are already registered", Toast.LENGTH_SHORT).show()
+      val intent = Intent(this, LoginActivity::class.java)
+      startActivity(intent)
+      finish()
+    }
+
 
 //    editTextEmail.doAfterTextChanged { text, start, before, count ->
 //      if(!text!!.contains("@")) {
