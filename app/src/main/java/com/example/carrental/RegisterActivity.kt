9dceb3743f2af30ecbfa49999165dd60035eaf15
Ozilele <90649604+Ozilele@users.noreply.github.com>
2023-05-21 11:29:32 +0200

    Firebase Login Register

--- app/src/main/java/com/example/carrental/RegisterActivity.kt
+++ app/src/main/java/com/example/carrental/RegisterActivity.kt
@@ -3,33 +3,52 @@ package com.example.carrental
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.core.widget.doAfterTextChanged
 import androidx.core.widget.doOnTextChanged
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
 
-  lateinit var textInputLayoutEmail : TextInputLayout
-  lateinit var textInputLayName : TextInputLayout
-  lateinit var textInputLayPassword : TextInputLayout
-  lateinit var textInputLayPhone : TextInputLayout
-  lateinit var registerBtn : Button
-  lateinit var alreadyregisteredBtn : Button
-  lateinit var editTextEmail  : TextInputEditText
-  lateinit var editTextName : TextInputEditText
-  lateinit var editTextPhone : TextInputEditText
-  lateinit var editTextPassword : TextInputEditText
+  private lateinit var textInputLayoutEmail : TextInputLayout
+  private lateinit var textInputLayName : TextInputLayout
+  private lateinit var textInputLayPassword : TextInputLayout
+  private lateinit var textInputLayPhone : TextInputLayout
+  private lateinit var registerBtn : Button
+  private lateinit var progressBar: ProgressBar
+  private lateinit var alreadyregisteredBtn : Button
+  private lateinit var editTextEmail  : TextInputEditText
+  private lateinit var editTextName : TextInputEditText
+  private lateinit var editTextPhone : TextInputEditText
+  private lateinit var editTextPassword : TextInputEditText
+  private lateinit var auth: FirebaseAuth
+
+  public override fun onStart() {
+    super.onStart()
+    // Check if user is signed in (non-null) and update UI accordingly.
+    auth.signOut()
+    val currentUser = auth.currentUser
+    if (currentUser != null) {
+      Toast.makeText(this, "You are already logged in", Toast.LENGTH_SHORT).show()
+    }
+  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_register)
 
+    auth = Firebase.auth
+
     registerBtn = findViewById(R.id.registerBtn)
     alreadyregisteredBtn = findViewById(R.id.btnAccAlready)
-
+    progressBar = findViewById(R.id.progressBar)
     textInputLayoutEmail = findViewById(R.id.textInputLayEmail)
     textInputLayName = findViewById(R.id.textInputLayName)
     textInputLayPassword = findViewById(R.id.textInputLayPassword)
@@ -40,53 +59,79 @@ class RegisterActivity : AppCompatActivity() {
     editTextPhone = findViewById(R.id.editTextPhone)
     editTextPassword = findViewById(R.id.editTextPassword)
 
-    editTextEmail.doAfterTextChanged {
-      println("${it.toString()}")
-    }
-
-    registerBtn.setOnClickListener{
-      var name = editTextName.text
-      val email = editTextEmail.text
-      val password = editTextPassword.text
-      val phoneNum = editTextPhone.text
-
-      if(password == null || password.length < 4) {
-        textInputLayPassword.error = "Invalid password(min 4 sybmols)"
-      }
-
-      if(phoneNum!!.length < 9 || phoneNum == null) {
-        textInputLayPhone.error = "Invalid phone number(9 numbers)"
-      }
-
-      if(name == null) {
-        textInputLayName.error = "Invalid field"
-      }
-      if(email == null || !email.contains("@")) {
-        textInputLayoutEmail.error = "Invalid email"
-      } else {
-        Toast.makeText(this, "You are registered", Toast.LENGTH_SHORT).show()
-        val intent = Intent(this, LoginActivity::class.java)
-        startActivity(intent)
-        finish()
-      }
-    }
-
     alreadyregisteredBtn.setOnClickListener{
-      Toast.makeText(this, "You are already registered", Toast.LENGTH_SHORT).show()
-      val intent = Intent(this, LoginActivity::class.java)
-      startActivity(intent)
-      finish()
+      moveToLoginPage()
+    }
+  }
+
+
+  private fun validateInputs() : Boolean {
+    var areInputsValid = true
+    var name = editTextName.text
+    val email = editTextEmail.text
+    val password = editTextPassword.text
+    val phoneNum = editTextPhone.text
+
+    if(password == null || password.length < 4) {
+      textInputLayPassword.error = "Invalid password(min 4 sybmols)"
+      areInputsValid = false
     }
 
+    if(phoneNum!!.length < 9 || phoneNum == null) {
+      textInputLayPhone.error = "Invalid phone number(9 numbers)"
+      areInputsValid = false
+    }
 
-//    editTextEmail.doAfterTextChanged { text, start, before, count ->
-//      if(!text!!.contains("@")) {
-//        textInputLayoutEmail.error = "Invalid email"
-//      } else {
-//        textInputLayoutEmail.error = null
-//      }
-//    }
+    if(name == null || name.length < 1) {
+      textInputLayName.error = "Invalid field"
+      areInputsValid = false
+    }
+    if(email == null || !email.contains("@")) {
+      textInputLayoutEmail.error = "Invalid email"
+      areInputsValid = false
+    }
 
+    return areInputsValid
+  }
+
+  fun onRegister(view: View) {
+    progressBar.visibility = View.VISIBLE
+    var validInputs = validateInputs()
+    var email = editTextEmail.text.toString()
+    var password = editTextPassword.text.toString()
+
+    println(email)
+    println(password)
+
+    if(validInputs) {
+      // Code to register the user in Firebase
+      auth.createUserWithEmailAndPassword(email, password)
+        .addOnCompleteListener(this) { task ->
+          progressBar.visibility = View.GONE
+          println(task)
+          if (task.isSuccessful) {
+            // Sign in success, update UI with the signed-in user's information
+//            val user = auth.currentUser
+            Toast.makeText(this, "Registered", Toast.LENGTH_SHORT).show()
+            moveToLoginPage()
+          } else {
+            // If sign in fails, display a message to the user.
+            Toast.makeText(
+              baseContext,
+              "Authentication failed.",
+              Toast.LENGTH_SHORT,
+            ).show()
+          }
+        }
+    } else {
+      Toast.makeText(this, "Inputs are not valid", Toast.LENGTH_SHORT).show()
+    }
+  }
+
+  private fun moveToLoginPage() {
+    val intent = Intent(this, LoginActivity::class.java)
+    startActivity(intent)
+    finish()
   }
 
 
